1a786159d6b8fa7e9b1ce29bc9bced521bb1fcef
Stephanie Lee <slee13@wellesley.edu>
2014-01-10 14:35:10 -0500

    Init RoR repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = '96a5e020f0609c047f152570d4d29d36d3158ab5ee74fc2f32f46068340a19703d4a179c67405fc6cbf044af14b5ddfe75f97903fad2ac686a901d61f4b66650'
